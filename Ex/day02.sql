e4d59a76b08ba8ff54f8d41950982929afec9e0c
clz2024.sun <clz2024.sun@gmail.com>
2024-07-19 14:28:01 +0900

    day02

--- Ex/day02.sql
+++ Ex/day02.sql
@@ -152,14 +152,90 @@ select *
 from employees;
 
 #월급이 10000 이상인 직원의 이름 월급을 월급이 큰직원부터 출력하세요
+select 	first_name,
+		salary
+from employees
+where salary >= 10000
+order by salary desc;
+
+select * from employees;
+
 
 #부서번호를 오름차순으로 정렬하고 부서번호가 같으면 월급이 높은 사람부터 
 #부서번호 월급 이름을 출력하세요  
+select 	department_id,
+		salary,
+        first_name
+from employees
+order by department_id asc, salary desc
+;
 
 
 #직원의 이름, 급여, 입사일을 이름의 알파벳 올림차순으로 출력하세요
-
+select 	first_name,
+		salary,
+        hire_date
+from employees
+order by first_name asc;
 
 #직원의 이름, 급여, 입사일을 입사일이 빠른 사람 부터 출력하세요
+select 	first_name,
+		salary,
+        hire_date
+from employees
+order by hire_date asc; 
+
+-- 실행순서 공부
+select 	first_name n,
+		salary s
+from employees
+where salary >= 10000
+order by s desc;     -- 가능
+
+-- 실행 안되는 이유 공부
+select 	first_name n,
+		salary s
+from employees
+where s >= 10000    -- 불가능
+order by salary desc;     
+
+/***************************************
+* 단일행 함수
+****************************************/
+-- 단일행함수 > 숫자함수
+
+-- round() : 반올림
+select 	round(123.123, 2),   -- 123.12 2째자리까지표현
+		round(123.126, 2),    -- 123.13 3째자리에서반올림
+        round(234.567, 0),     -- 235
+        round(123.456, 0),     -- 123
+        round(123.456),        -- 123
+        round(123.126, -1),    -- 120
+        round(125.126, -1),     -- 130
+        round(125.126, -2)     -- 100
+from dual;
+
+-- ceil() : 올림
+select 	ceil(123.456),
+		ceil(123.789),
+        ceil(123.7892313),
+        ceil(987.1234)
+from dual;
+
+-- floor() : 내림
+select 	floor(123.456),
+		floor(123.789),
+        floor(123.7892313),
+        floor(987.1234)
+from dual;
+
+-- truncate(): 자리수 버림
+select 	truncate(1234.34567, 2),		
+		truncate(1234.34567, 3),
+        truncate(1234.34567, 0),
+        truncate(1235.34567, -2)
+from dual;
+
+
 
 
